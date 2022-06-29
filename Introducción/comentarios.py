659a6a53e4cff80e2881a6ab5fb17bb127aefaaf
Diego Saavedra <dsaavedra88@gmail.com>
2022-06-28 22:38:59 -0500

    Introducción

---
+++ Introducción/comentarios.py
@@ -0,0 +1,8 @@
+"""
+Esto es un script de Python
+más de una linea
+"""
+#Esto es un comentario de una linea
+
+
+print("Hola Mundo")  #Esto es una instrucción para escribir hola mundo en python
---
+++ Introducción/variables.py
@@ -0,0 +1,16 @@
+"""
+Aquí van algunos ejemplos de variables
+"""
+#a = "Diego Saavedra" #Esto no se debe hacer
+#print(a)
+
+nombre = "Diego Saavedra" #Esto si se debe hacer
+print(nombre)
+
+email = "diegosaavedra@gmail.com"
+
+telefono = 09921458756
+_nombrepersona = "Juan"
+
+persona1 = "Pedro"
+personaAdulta = "Maria Jose"
